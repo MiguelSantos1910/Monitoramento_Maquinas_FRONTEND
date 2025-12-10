20e48bf2918eacc2f848f7fee0055da7edc2bd40
MiguelSantos1910 <miguelsantos51346@gmail.com>
2025-12-10 19:59:50 -0300

    Add files via upload

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api'
+    baseURL: 'monitoramento_maquinas_backend.railway.internal'
 });
 export default api;
